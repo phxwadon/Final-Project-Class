597577c4091a03a76bc21680b2aa70eec5b1bdd4
phxwadon <somtui1412@gmail.com>
2026-02-03 19:58:49 +0700

    Setup Angular frontend and Backend complete

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -10,4 +10,4 @@ const ProductSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-MediaSourceHandle.exports = mongoose.model('Product', ProductSchema)
+module.exports = mongoose.model('Product', ProductSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Product = require("./models/Product");
 require('dotenv').config();
+const productRoutes = require('./routes/product.Routes')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const mongoURL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017';
+app.use('/api/products', productRoutes);
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/Final-project-class';
 mongoose.connect(mongoURL)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
